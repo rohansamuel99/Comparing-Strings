e8c945c36e03ecd357f63b5a2bda6bcbdff89864
Rohan Hariharan <rohan-samuel.hariharan@agilent.com>
2022-01-17 10:45:11 +0000

    Added ideas to compare distance

---
+++ CompareDistance.py
@@ -0,0 +1,4 @@
+# take 2 strings and assign a number to k and split them in rolling k's or normal spilts. 
+# take the k-mers from both the strings and compare them side by side, and then use the number of characters between 2 similar k-mers from the strings 
+# to find the distance between the k-mers
+# this can be done by assigning the strings to a list and then parsing through the list to find k-mers 
