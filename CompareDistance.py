a633624e17ca7be90c14bfce3de027412458ac16
Rohan Hariharan <rohan-samuel.hariharan@agilent.com>
2022-03-16 16:04:25 +0000

    got it to work with a dictionary but it adds similar kmers from different strings, need it to subtract that difference

--- CompareDistance.py
+++ CompareDistance.py
@@ -1,54 +1,43 @@
+# initial ideas
 # take 2 strings and assign a number to k and split them in rolling k's or normal spilts. 
 # take the k-mers from both the strings and compare them side by side, and then use the number of characters between 2 similar k-mers from the strings 
 # to find the distance between the k-mers
 # this can be done by assigning the strings to a list and then parsing through the list to find k-mers 
-
-def get_q_gram_distance(s, t, kmers, k):
-    #strings s, t of length n and m respectively
-    n=len(s)
-    m=len(t)
-    #an empty array 
-    d=[]
-    for i in range(len(kmers)):
-        d.append(0)#init vector, O(q)
-        
-    for i in range(1, n - k + 1):#slide window, O(n)
-        for r in range(0, len(kmers)):#get count, O(q)
-            if s[i:i + k - 1] == kmers[r]:
-                d[r]+=1#first vector sends postive effect
-        
-                    
-    for i in range(1, m - k + 1):#slide window, O(m)
-        for r in range(0,len(kmers)):#get count, O(q)
-            if s[i:i + k - 1] == kmers[r]:
-                d[r] -= 1#second vector sends negtive effect
-    res = 0
-    for i in vector:
-        res += abs(i)
-        
-    return res
 
 # the monstrosity
 qgram_distance2 = lambda s, t, k: sum(abs({z:sum((1 if a==z else 0)for a in[s[i:i+k]for i in range(len(s)-k+1)])-sum((1 if b==z else 0)for b in[t[i:i+k] for i in range(len(t)-k+1)])for z in{q:0 for q in[s[i:i+k]for i in range(len(s)-k+1)]+[t[i:i+k]for i in range(len(t)-k+1)]}}[key])for key in{z:sum((1 if a==z else 0)for a in[s[i:i+k]for i in range(len(s)-k+1)])-sum((1 if b==z else 0)for b in[t[i:i+k]for i in range(len(t)-k+1)])for z in{q:0 for q in[s[i:i+k]for i in range(len(s)-k+1)]+[t[i:i+k]for i in range(len(t)-k+1)]}})
 # s = string 1 from dna_string.txt
 # t = string 2 from dna_string.txt
 # k = length of k-mer wanted
+
+# using a dictionary
  
-def kmer_distance(s, t, k, kmer):
-    
+def kmer_distance(s, t, k):
+    #empty dictionary
     d = {}
 
-    for i in range(1, len(s) - k + 1):
-        # for z in range(0, k):
-        s[i : i +  - 1] == kmer
-            # d += 1
-        if kmer.has_key(kmer):
-            kmer[kmer] += 1
-        else:
-            kmer[kmer] = 1
-    # for j in range(1, len(t) - k + 1):
+    for i in range(len(s) - k + 1):
+        kmer = s[i:i + k]
+        if kmer not in d:
+            d[kmer] = 0
+        d[kmer] += 1
+    #     for z in range(0, k):
+    #        if s[i : i + k - 1] == k:
+    #            d += 1
+    
+    for j in range(len(t) - k + 1):
+        kmer = t[j:j + k]
+        if kmer not in d:
+            d[kmer] = 0
+        d[kmer] += 1
     #     for z in range(0, k):
     #         if s[i : i + k - 1] == k:
     #             d -= 1
+    # return d
+    return abs(sum(d.values()))
+    
 
-print(kmer_distance("AGCT", "ACGT", 2, 1))
+# print(kmer_distance("AGCT", "ACGT", 2))
+print(kmer_distance("GCTAGCTAGCAT", "ACGATCGATCGA", 2))
+
+#### adds similar kmers from different strings, need it to subtract that difference
